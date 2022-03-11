0101512ed414617cbbdb1dcb5a08ab352b8e6478
KXLAA <kolade.afode@yahoo.com>
2022-03-12 00:11:48 +0100

    feat: add og images and meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import '@/styles/global.scss';
 import { createGlobalStyle } from 'styled-components';
+import Head from 'next/head';
 
 const GlobalStyle = createGlobalStyle`
 *, *::before, *::after {
@@ -41,6 +42,67 @@ const GlobalStyle = createGlobalStyle`
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Frontend Cardio 💪</title>
+        <meta
+          name="description"
+          content="Kola's catalogue of projects, challenges & experiments"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#101010" />
+        <meta name="msapplication-TileColor" content="#101010" />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://frontendcardio.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Frontend Cardio 💪" />
+        <meta
+          property="og:description"
+          content="Kola's catalogue of projects, challenges & experiments"
+        />
+        <meta
+          property="og:image"
+          content="https://ucarecdn.com/802399d8-ae9d-4f26-a00a-fa99a1bdbc2b/Group59.png"
+        />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="https://frontendcardio.vercel.app/"
+        />
+        <meta
+          property="twitter:url"
+          content="https://frontendcardio.vercel.app/"
+        />
+        <meta name="twitter:title" content="Frontend Cardio 💪" />
+        <meta
+          name="twitter:description"
+          content="Kola's catalogue of projects, challenges & experiments"
+        />
+        <meta
+          name="twitter:image"
+          content="https://ucarecdn.com/802399d8-ae9d-4f26-a00a-fa99a1bdbc2b/Group59.png"
+        />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
